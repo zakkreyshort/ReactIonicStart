5367b8144bc55b12a3809c08d379ef8c766bc57e
Zakkrey Short <zakkreyjshort@gmail.com>
2020-06-04 14:54:53 -0700

    create tab3 and fill with components

---
+++ src/components/Tab3/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { IonApp, IonContent, IonCard, IonList, IonTabs, IonHeader, IonCardHeader, IonCardTitle, IonCardSubtitle, IonRouterOutlet, IonTabBar, IonTabButton, IonIcon, IonLabel, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonSelectOption, IonSelect, IonItem, IonDatetime } from '@ionic/react';
+
+export default class Tab3 extends React.Component {
+    render() {
+        return (<>
+        <IonHeader>
+            <IonToolbar>
+                <IonButtons>
+                    <IonMenuButton></IonMenuButton>
+                </IonButtons>
+                <IonTitle>Tab 1</IonTitle>
+            </IonToolbar>
+        </IonHeader>
+
+        <IonContent>
+            <div className="ion-padding about-info">
+                <h4>Title goes here</h4>
+
+                <IonList lines="none">
+                    <IonItem>
+                        <IonIcon name="calendar" slot="start"></IonIcon>
+                        <IonLabel>Date Picker!</IonLabel>
+                        <IonDatetime displayFormat="MMM DD, YYYY" max="2056" value={null}></IonDatetime>
+                    </IonItem>
+
+                    <IonItem>
+                        <IonIcon name="pin" slot="start"></IonIcon>
+                        <IonLabel>Selector</IonLabel>
+                        <IonSelect>
+                            <IonSelectOption value="portland">Portland, Or</IonSelectOption>
+                            <IonSelectOption value="denver">Denver, Co</IonSelectOption>
+                            <IonSelectOption value="seattle">Seattle, Wa</IonSelectOption>
+                            <IonSelectOption value="sf">San Francisco, Ca</IonSelectOption>
+                        </IonSelect>
+                    </IonItem>
+                </IonList>
+
+                <p>
+                    Could add some stuff here for fun!
+                </p>
+
+            </div>
+        </IonContent>
+        </>)
+    }
+}
