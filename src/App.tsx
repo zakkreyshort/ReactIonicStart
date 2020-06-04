e07ef66f4c792f5672a5b03d89028fdec7341dfa
Zakkrey Short <zakkreyjshort@gmail.com>
2020-06-04 13:01:05 -0700

    successfully add ionic components to app, renders.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import '@ionic/core/css/core.css';
+import '@ionic/core/css/ionic.bundle.css';
 import './App.css';
+import { IonApp, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle } from '@ionic/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <IonApp>
+        <IonContent>
+          <IonCard>
+            <IonCardHeader>
+              <IonCardSubtitle>Welcome to my Ionic-React App!</IonCardSubtitle>
+              <IonCardTitle>ZS</IonCardTitle>
+            </IonCardHeader>
+          </IonCard>
+        </IonContent>
+      </IonApp>
+    );
+  }
 }
 
 export default App;
