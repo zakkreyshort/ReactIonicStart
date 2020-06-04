32c3c20ffbe391a5bf0847598260e57294a95131
Zakkrey Short <zakkreyjshort@gmail.com>
2020-06-04 13:18:20 -0700

    create ion page with tabs and buttons for each view

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { IonApp, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle } from '@ionic/react';
+import { IonApp, IonContent, IonCard, IonTabs, IonCardHeader, IonCardTitle, IonCardSubtitle, IonRouterOutlet, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/react';
 import { Route } from 'react-router-dom';
 import { IonPage } from '@ionic/react';
 import Tab1 from '../Tab1';
@@ -10,7 +10,31 @@ class Home extends Component {
     render() {
         return (
             <IonApp>
-                
+                <IonPage>
+                    <IonTabs>
+                        <IonRouterOutlet>
+                            <Route />
+                            <Route />
+                            <Route />
+                        </IonRouterOutlet>
+
+                        <IonTabBar>
+                            <IonTabButton>
+                                <IonIcon />
+                                <IonLabel></IonLabel>
+                            </IonTabButton>
+                            <IonTabButton>
+                                <IonIcon />
+                                <IonLabel></IonLabel>
+                            </IonTabButton>
+                            <IonTabButton>
+                                <IonIcon />
+                                <IonLabel></IonLabel>
+                            </IonTabButton>
+                        </IonTabBar>
+
+                    </IonTabs>
+                </IonPage>
             </IonApp>
         )
     }
